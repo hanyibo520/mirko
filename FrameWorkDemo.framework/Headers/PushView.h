d36b1d0bc406e4908206265f35b5f4ee2500f504
mirko <182423535363@163.com>
2024-09-10 20:19:02 +0800

    新项目

---
+++ FrameWorkDemo.framework/Headers/PushView.h
@@ -0,0 +1,17 @@
+//
+//  PushView.h
+//  FrameWorkDemo
+//
+//  Created by admin on 2024/9/3.
+//
+
+#import <UIKit/UIKit.h>
+#import <FrameWorkDemo/UIView+YYadd.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PushView : UIView
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FrameWorkDemo.framework/Headers/UIView+YYadd.h
@@ -0,0 +1,16 @@
+//
+//  UIView+YYadd.h
+//  FrameWorkDemo
+//
+//  Created by admin on 2024/9/3.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (YYadd)
+- (UIViewController *)viewController;
+@end
+
+NS_ASSUME_NONNULL_END
